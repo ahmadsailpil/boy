86ceb050dc464668034b6ba9b7cca003db1c2d60
dawawasgg <pnbsave@gmail.com>
2022-07-17 16:42:13 +0800

    xop

---
+++ plugins/info-script.js
@@ -0,0 +1,25 @@
+//Credits Jangan Dihapus
+//Thanks For KannaChan & Papah-Chan
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://youtube.com/channel/UCI0e4GqmeLZhy8KE8c9JPsw*`
+let wibu = `https://api.zacros.my.id/randomimg/waifu` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/ohlx_store",
+    mediaType: "VIDEO",
+    description: "https://chat.whatsapp.com/IIpo1Rq4oXH7MbkPYTWX4E", 
+    title: 'ridho',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
